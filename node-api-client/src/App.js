9e7c086624fa9c5be9453a9983f18a58a787d2e6
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2019-10-20 18:55:40 -0400

    data displays

--- node-api-client/src/App.js
+++ node-api-client/src/App.js
@@ -3,10 +3,24 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [userData, setUserData] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:5000/api/users/')
+      .then(res => setUserData(res.data))
+      .catch(err => console.log(err))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-
+        {userData.map(user => {
+          return <div className="user-div" key={user.id}>
+            <p>{user.name}</p>
+            <p>{user.bio}</p>
+          </div>
+        })}
       </header>
     </div>
   );
