4b5eb44083655bc5ba7ca95c9052b874fffc99f1
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2019-10-20 18:10:01 -0400

    get by ID

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const server = express();
 server.use(express.json());
 
 server.get('/api/users', (req, res) => {
-  console.log(req.body)
   userList
     .find()
     .then(user => res.send(user))
@@ -14,30 +13,29 @@ server.get('/api/users', (req, res) => {
 });
 
 server.get('/api/users/:id', (req, res) => {
-  console.log(req.params)
   if (!req.params.id) res.status(400).send({ message: "Your request is missing the user id."});
   if (req.params.id === undefined) res.status(404).send({ message: "The user with the specified ID does not exist." });
 
-  userList = userList.map(user => {
-    if (`${user.id}` === req.params.id) {
-      return req.body;
-    }
-    return user;
-  });
-  res.status(200).send(req.body);
+  userList
+    .findById(req.params.id)
+    .then(user => res.status(200).json(user))
+    .catch(err => res.status(500).send({ error: "The user information could not be retrieved." }))
 })
 
 server.post('/api/users', (req, res) => {
   if (!req.body.bio || !req.body.name) {
-    res.status(400).send({ errorMessage: "Please provide name and bio for the user." })
+    res.status(400).send({ message: "Please provide name and bio for the user." })
   }
 
-  console.log('user data', req.body.bio);
+  console.log('user data', req.body);
 
   userList
-    .insert(userData)
-    .then(user => res.json(user))
-    .catch(err => res.json({ message: 'Unable to add user.' }))
+    .insert(req.body)
+    .then(user => {
+      console.log(user)
+      res.status(201).send({ message: `${req.body.name} has been added.`})
+    })
+    .catch(err => res.status(500).send({ error: "There was an error while saving the user to the database" }))
 });
 
 // server.put('/api/users/:id', (req, res) => {
